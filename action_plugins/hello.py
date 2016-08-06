d298a774f3feae79025589b50a42d81eabab334b
Henry Finucane <h.finucane+gh@gmail.com>
2016-08-06 10:36:24 -0700

    Update 'hello world'

--- action_plugins/hello.py
+++ action_plugins/hello.py
@@ -1,11 +1,5 @@
-from ansible.runner.return_data import ReturnData
+from ansible.plugins.action import ActionBase
 
-
-class ActionModule(object):
-    def __init__(self, runner):
-        self.runner = runner
-
-    def run(self, conn, tmp, module_name, module_args, inject, complex_args=None, **kwargs):
-        return ReturnData(conn=conn,
-                          comm_ok=True,
-                          result=dict(failed=False, changed=False, msg="Hello World"))
+class ActionModule(ActionBase):
+    def run(self, tmp=None, task_vars=None):
+        return dict(msg="Hello World", _ansible_verbose_always=True)
